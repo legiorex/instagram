3fda10c4dbce0fbbc59aec2fe75a93b3d2b6ff02
Dima Shvedsky <imagnum.satellite@gmail.com>
2018-07-13 23:57:58 +0300

    chore(source): update main favicon

--- source/components/Photo.js
+++
@@ -1,6 +0,0 @@
-// Core
-import React from 'react';
-
-export default (props) => {
-    return props.secured ? null : <img className = 'photo' src = { props.src } />;
-};
--- source/index.js
+++ source/index.js
@@ -5,7 +5,20 @@ import ReactDOM from 'react-dom';
 // Theme
 import './theme/init';
 
-// App
-import Instagram from './pages/Instagram';
+const start = (
+    <h1
+        style = { {
+            display:         'flex',
+            justifyContent:  'center',
+            alignItems:      'center',
+            minHeight:       '100vh',
+            backgroundColor: '#070A13',
+            color:           'white',
+            fontSize:        24,
+            fontWeight:      '600',
+        } }>
+        Добро пожаловать на воркшоп по React.
+    </h1>
+);
 
-ReactDOM.render(<Instagram />, document.getElementById('app'));
+ReactDOM.render(start, document.getElementById('app'));
