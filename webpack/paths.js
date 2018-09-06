7b02ef6d0f5b647cbafb81eb8951fba07a85703f
Dima Shvedsky <d.vakatsiienko@lectrum.io>
2018-09-06 16:42:35 +0300

    feat(source): extract environment to generator

--- webpack/paths.js
+++
@@ -1,7 +0,0 @@
-// Instruments
-import { resolve } from 'path';
-
-// Core
-export const SOURCE = resolve(__dirname, '../source');
-export const BUILD = resolve(__dirname, '../build');
-export const STATICS = resolve(__dirname, '../static');
