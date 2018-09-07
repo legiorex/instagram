94093043e7143f36628a7b10ad320b8f4d07dd9a
Dima Shvedsky <d.vakatsiienko@lectrum.io>
2018-09-07 16:19:13 +0300

    refactor(source): improve entry point code style

--- source/index.js
+++ source/index.js
@@ -7,17 +7,17 @@ import './theme/init';
 
 const start = (
     <h1
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            minHeight: '100vh',
+        style = {{
+            display:         'flex',
+            justifyContent:  'center',
+            alignItems:      'center',
+            minHeight:       '100vh',
             backgroundColor: '#070A13',
-            color: 'white',
-            fontSize: 24,
-            fontWeight: '600',
-            textAlign: 'center',
-            margin: 0,
+            color:           'white',
+            fontSize:        24,
+            fontWeight:      '600',
+            textAlign:       'center',
+            margin:          0,
         }}>
         Добро пожаловать на воркшоп по React
     </h1>
