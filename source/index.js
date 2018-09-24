1145286913353c8062596b67affe672ddeb9294c
Dima Shvedsky <d.vakatsiienko@lectrum.io>
2018-09-24 11:24:06 +0300

    chore(source): separate entrypoint styles

--- source/index.js
+++ source/index.js
@@ -5,22 +5,6 @@ import ReactDOM from 'react-dom';
 // Theme
 import './theme/init';
 
-const start = (
-    <h1
-        style = {{
-            display:         'flex',
-            justifyContent:  'center',
-            alignItems:      'center',
-            minHeight:       '100vh',
-            backgroundColor: '#070A13',
-            color:           'white',
-            fontSize:        24,
-            fontWeight:      '600',
-            textAlign:       'center',
-            margin:          0,
-        }}>
-        Добро пожаловать на воркшоп по React
-    </h1>
-);
+const start = <div className = 'start'>Добро пожаловать!</div>;
 
 ReactDOM.render(start, document.getElementById('app'));
